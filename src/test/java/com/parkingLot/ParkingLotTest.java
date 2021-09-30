e96b9609d1192f73b83d8593805341cf31e6bb99
sheetal-10 <sheetalp257@gmail.com>
2021-09-30 20:14:55 +0530

    Refactored : test cases for exception handling

--- src/test/java/com/parkingLot/ParkingLotTest.java
+++ src/test/java/com/parkingLot/ParkingLotTest.java
@@ -1,29 +1,35 @@
 package com.parkingLot;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ParkingLotTest {
+
+    ParkingLotSystem parkingLotSystem = null;
+    Object vehicle = null;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        vehicle = new Object();
+        parkingLotSystem = new ParkingLotSystem();
+    }
+
     @Test
     public void givenAVehicle_WhenParked_ShouldReturnTrue() {
-        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
-        boolean isParked =parkingLotSystem.park(new Object());
+        boolean isParked = parkingLotSystem.park(new Object());
         Assertions.assertTrue(isParked);
     }
 
     @Test
-    public void givenAVehicle_WhenUnParked_ShouldReturnTrue(){
-        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
-        Object vehicle = new Object();
+    public void givenAVehicle_WhenUnParked_ShouldReturnTrue() {
         parkingLotSystem.park(vehicle);
         boolean isUnParked = parkingLotSystem.unPark(vehicle);
         Assertions.assertTrue(isUnParked);
     }
 
     @Test
-    public void givenAVehicle_WhenAlreadyParked_ShouldReturnFalse(){
-        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
-        Object vehicle = new Object();
+    public void givenAVehicle_WhenAlreadyParked_ShouldReturnFalse() {
         parkingLotSystem.park(vehicle);
         boolean isParked = parkingLotSystem.park(new Object());
         Assertions.assertFalse(isParked);
